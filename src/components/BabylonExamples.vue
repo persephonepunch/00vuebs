c6f6c42877cce1157a37a6015febb28245a4ca44
yoonsun lee <yoonsunlee150@gmail.com>
2022-06-01 20:25:52 -0400

    vue js babylon scaffold

---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,27 @@
+<template>
+<h3>Babylon</h3>
+<Canvas>
+
+</Canvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { BasicScene } from '@/BabylonExamples/BasicScene';
+
+export default defineComponent({
+  name: 'BabylonExamples',
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    new BasicScene(canvas);
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas {
+  width:70%;
+  height:70%;
+}
+</style>
